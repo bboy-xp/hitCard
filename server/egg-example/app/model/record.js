28c8dd76a1dd2e7df35bfc6dedb5179a6d38f0cd
xp-12138 <1169470657@qq.com>
2018-03-12 17:38:11 +0800

    测试vscode git是否好使

--- server/egg-example/app/model/record.js
+++ server/egg-example/app/model/record.js
@@ -6,7 +6,8 @@ module.exports = app => {
       createTime: {type: Date},
       hitCardTime: {type: Date},
       use: {type: Boolean},//记录支付的一元钱是否被用了，hitCard时更新
-      getMoney: {type: Number}//获得的钱，最后打完卡更新
+      getMoney: {type: Number},//获得的钱，最后打完卡更新
+      got: {type: Boolean}//是否获得了钱，先在hitCard初始化，再在successHitCard时更新
       
     },{
       timestamps: true,
